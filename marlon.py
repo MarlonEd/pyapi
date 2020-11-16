33e216b4b42b0dbbd47b0364b4a0ad456cd65786
Marlon <marlon.edmond@gmail.com>
2020-11-16 19:50:20 +0000

    learning about lists

---
+++ marlon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# SHEBANG Line
+
+
+
+# Lets take some input from the user!
+name= input("What is your name? ")
+
+week= input("What is the day of the week? ")
+
+red= "Hello "
+
+
+# orange= "! Welcome to Python!"
+
+print(red + name + "Happy " + week + "!")
